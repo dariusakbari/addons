273da04a4cf08f33475aacc5ccb3879d33188507
Darius Akbari <d.akbari@hotmail.com>
2026-07-24 17:52:23 -0400

    Add gte_hub: SmartBuild-style project-centric tabs on the project form
    RFIs, Change Orders, Submittals, and Field & Safety (daily logs, FLHAs,
    incidents, NCRs) embedded as pages inside each project. Records created
    from within a project auto-link to it.

---
+++ gte_hub/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "GTE Project Hub",
+    "summary": "SmartBuild-style project-centric navigation: all construction records inside the project form",
+    "version": "19.0.0.1.0",
+    "author": "Green Tech Electric",
+    "license": "OPL-1",
+    "category": "Construction",
+    "depends": ["gte_controls", "gte_field", "gte_hse"],
+    "data": [
+        "views/project_hub_views.xml",
+    ],
+    "installable": True,
+}
